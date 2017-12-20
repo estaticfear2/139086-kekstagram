e0a7a52a046ae23b2efece1b9869088305b7020b
Slava Slivka <aphelion.unet@gmail.net>
2017-12-20 15:52:51 +0200

    m7-t1

--- js/gallery.js
+++ js/gallery.js
@@ -4,6 +4,7 @@
   var galleryOverlay = document.querySelector('.gallery-overlay');
   var galleryClose = galleryOverlay.querySelector('.gallery-overlay-close');
   var pictures = document.querySelector('.pictures');
+  var filters = document.querySelector('.filters');
 
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
@@ -24,11 +25,61 @@
     document.removeEventListener('keydown', onOverlayPreviewEscPress);
   };
 
+  var PHOTOS = null;
+
   var renderPictures = function (data) {
     pictures.appendChild(window.picture.collectPhotos(data, window.picture.renderPhoto));
+
+    if (!PHOTOS) {
+      PHOTOS = data;
+      PHOTOS.forEach(function (item, i) {
+        item.commentsLength = item.comments.length;
+        item.recommend = -i;
+      });
+      filters.classList.remove('filters-inactive');
+    }
+  };
+
+  var filter = 'recommend';
+
+  var onFilterChange = function (evt) {
+    var FILTER = {
+      'filter-recommend': 'recommend',
+      'filter-popular': 'likes',
+      'filter-discussed': 'commentsLength',
+      'filter-random': 'filter-random'
+    };
+
+    if (evt.target.tagName === 'INPUT') {
+      if (filter !== FILTER[evt.target.id]) {
+        filter = FILTER[evt.target.id];
+      } else {
+        return;
+      }
+
+      window.globals.clearElem(pictures);
+
+      var byField = function (field) {
+        if (arguments[0] === 'filter-random') {
+          return function () {
+            return Math.random() - 0.5;
+          };
+        } else {
+          return function (a, b) {
+            return a[field] > b[field] ? -1 : 1;
+          };
+        }
+      };
+
+      renderPictures(PHOTOS.sort(byField(filter)));
+    }
   };
 
   window.backend.load(renderPictures, window.showOnLoadError);
+
+  filters.addEventListener('click', function (evt) {
+    window.globals.debounce(onFilterChange, evt);
+  });
 
   pictures.addEventListener('click', function (evt) {
     evt.preventDefault();
--- js/globals.js
+++ js/globals.js
@@ -1,23 +1,38 @@
 'use strict';
 
 (function () {
-  window.showOnLoadError = function (errorMessage) {
-    var node = document.createElement('div');
-    node.style.marginTop = 0;
-    node.style.marginLeft = 'auto';
-    node.style.marginRight = 'auto';
-    node.style.padding = '10px';
-    node.style.position = 'absolute';
-    node.style.width = '70%';
-    node.style.textAlign = 'center';
-    node.style.left = 0;
-    node.style.right = 0;
-    node.style.fontSize = '20px';
-    node.style.color = 'black';
-    node.style.backgroundColor = 'rgba(255, 255, 255, .5)';
-    node.style.zIndex = 100;
+  var lastTimeout;
 
-    node.textContent = errorMessage;
-    document.body.insertAdjacentElement('afterbegin', node);
+  window.globals = {
+    showOnLoadError: function (errorMessage) {
+      var node = document.createElement('div');
+      node.style.marginTop = 0;
+      node.style.marginLeft = 'auto';
+      node.style.marginRight = 'auto';
+      node.style.padding = '10px';
+      node.style.position = 'absolute';
+      node.style.width = '70%';
+      node.style.textAlign = 'center';
+      node.style.left = 0;
+      node.style.right = 0;
+      node.style.fontSize = '20px';
+      node.style.color = 'black';
+      node.style.backgroundColor = 'rgba(255, 255, 255, .5)';
+      node.style.zIndex = 100;
+
+      node.textContent = errorMessage;
+      document.body.insertAdjacentElement('afterbegin', node);
+    },
+    clearElem: function (elem) {
+      for (var i = elem.children.length - 1; i >= 0; i--) {
+        elem.removeChild(elem.children[i]);
+      }
+    },
+    debounce: function (action, context) {
+      if (lastTimeout) {
+        clearTimeout(lastTimeout);
+      }
+      lastTimeout = setTimeout(action, 500, context);
+    }
   };
 })();
